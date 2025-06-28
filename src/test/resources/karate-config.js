d70dc68bb1f06c9a699e980c6204528d48ec6e46
DS\MohammAb <abdul.mohammad@acc.co.nz>
2025-06-28 15:06:04 +1200

    Added minor change to the post deployment test and rollback if needed

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -5,7 +5,7 @@ function fn() {
 
     karate.log('karate.env system property was:', env);
 
-    if (env === 'test') {
+    if (env === 'dev') {
         config.baseUrl = 'http://intflow-service-dev.apps.crc.testing.com:8080';
     } else if (env === 'local') {
         config.baseUrl = 'http://localhost:8080';
